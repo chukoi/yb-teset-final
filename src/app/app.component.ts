a97f43812c3cfc09b1d5effc21f2f7df14dea441
Dean Heffernan <dean.heffernan@nextpracticehealth.com>
2025-03-23 17:29:25 +1100

    Upgraded to working condition again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@
  */
 
 // Imports.
-import {Component, OnInit} from '@angular/core';
-import {RichListService} from '../services/richList.service';
-import {_} from 'underscore'; // Use Lodash next time. Wanted to reminisce on the old school days: )
-import camelize from '../helpers/camelize';
-import arrayRemove from '../helpers/arrayRemove';
+import { Component, OnInit } from "@angular/core";
+import { RichListService } from "../services/richList.service";
+import _ from "underscore"; // Use Lodash next time. Wanted to reminisce on the old school days: )
+import camelize from "../helpers/camelize";
+import arrayRemove from "../helpers/arrayRemove";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   providers: [RichListService],
 })
 export class AppComponent implements OnInit {
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
   referenceLink: string; // Reference link.
   dollarValues: object; // Object to hold information about the different currencies.
   celebrityList: Array<object>; // Main list of richest celebrities.
-  celebrityClone: Array<object>; // Clone of  main list.
+  celebrityClone: Array<object> = []; // Clone of  main list.
   countries: Array<string>; // Birth place options.
   currencies: Array<string>; // Currency options.
   orders: Array<string>; // Order options.
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
   currencyClause: string; // Selected currency.
   searchClause: string; // Current search string.
   orderClause: string; // Select order by.
-  currencyPrefix = '$USD'; // Initial currency prefix.
+  currencyPrefix = "$USD"; // Initial currency prefix.
 
   /**
    * constructor
@@ -42,8 +42,7 @@ export class AppComponent implements OnInit {
    * @param {RichList} richListService
    * @return {void}
    */
-  constructor(private richListService: RichListService) {
-  }
+  constructor(private richListService: RichListService) {}
 
   /**
    * Initialise component
@@ -52,7 +51,7 @@ export class AppComponent implements OnInit {
    */
   ngOnInit() {
     // Retrive the data for the page.
-    this.richListService.getData().subscribe(data => {
+    this.richListService.getData().subscribe((data) => {
       // Initialise all the data.
       this.initData(data);
       this.initClauses();
@@ -68,10 +67,10 @@ export class AppComponent implements OnInit {
    * @return {void}
    */
   private initClauses() {
-    this.birthClause = 'Show All';
-    this.currencyClause = 'US Dollar';
-    this.searchClause = '';
-    this.orderClause = 'Rank';
+    this.birthClause = "Show All";
+    this.currencyClause = "US Dollar";
+    this.searchClause = "";
+    this.orderClause = "Rank";
   }
 
   /**
@@ -83,13 +82,13 @@ export class AppComponent implements OnInit {
   private initData(data) {
     // Map data to models. We skip the dollar value keys as we build a custom object for them.
     for (const key in data) {
-      if (data.hasOwnProperty(key) && !key.includes('Value')) {
+      if (data.hasOwnProperty(key) && !key.includes("Value")) {
         this[key] = data[key];
       }
     }
     // We create a new key to hold the converted currency as we don't want to mutate the original.
     this.celebrityList.map(function (value) {
-      value['netWorthConvert'] = value['netWorth'];
+      value["netWorthConvert"] = value["netWorth"];
       return value;
     });
     // We clone the celebrity list so we don't mutate it as filters are applied.
@@ -99,16 +98,16 @@ export class AppComponent implements OnInit {
     this.dollarValues = {
       usDollar: {
         value: data.usDollarValue,
-        prefix: '$USD'
+        prefix: "$USD",
       },
       australianDollar: {
         value: data.australianDollarValue,
-        prefix: '$AUD'
+        prefix: "$AUD",
       },
       euro: {
         value: data.euroValue,
-        prefix: '$EUR'
-      }
+        prefix: "$EUR",
+      },
     };
   }
 
@@ -119,20 +118,25 @@ export class AppComponent implements OnInit {
    */
   private setCountryOptions() {
     // Dynamically set countries from celebrity list values. Ideally we should pull this data from a service.
-    this.countries = _.chain(this.celebrityList).map(function (item) {
-      return item.country;
-    }).uniq().value().sort(function (a, b) {
-      // Sort birth place list alphabetically to make it easier for the user to navigate.
-      if (a < b) {
-        return 1;
-      }
-      if (b < a) {
-        return -1;
-      }
-      return 0;
-    }).reverse();
+    this.countries = _.chain(this.celebrityList)
+      .map(function (item) {
+        return item.country;
+      })
+      .uniq()
+      .value()
+      .sort(function (a, b) {
+        // Sort birth place list alphabetically to make it easier for the user to navigate.
+        if (a < b) {
+          return 1;
+        }
+        if (b < a) {
+          return -1;
+        }
+        return 0;
+      })
+      .reverse();
     // Add option to unset filter.
-    this.countries.unshift('Show All');
+    this.countries.unshift("Show All");
   }
 
   /**
@@ -144,23 +148,26 @@ export class AppComponent implements OnInit {
     // Get currency options from the dollarValues keys.
     const currencies = _.map(this.dollarValues, function (value, key) {
       return key.replace(/([A-Z])/g, function ($1) {
-        return ' ' + $1;
+        return " " + $1;
       });
     });
     this.currencies = currencies.map(function (value) {
       let string;
       // When we build the options they first letter is not capitalized. Fix that.
-      if (value === 'us Dollar') {
+      if (value === "us Dollar") {
         // Hack to fix Us to US.
-        string = value.charAt(0).toUpperCase() + value.charAt(1).toUpperCase() + value.substr(2);
+        string =
+          value.charAt(0).toUpperCase() +
+          value.charAt(1).toUpperCase() +
+          value.substr(2);
       } else {
         string = value.charAt(0).toUpperCase() + value.substr(1);
       }
       // Remove "Value" from the option.
-      return string.replace('Value', '');
+      return string.replace("Value", "");
     });
     // We don't wan't the "Net Worth Convert" to be an options.
-    delete this.currencies['netWorthConvert'];
+    delete this.currencies["netWorthConvert"];
   }
 
   /**
@@ -172,7 +179,7 @@ export class AppComponent implements OnInit {
     // Dynamically built order options from keys in celebrity list. Bit hacky here because celebrity list might be empty.
     const orders = _.map(this.celebrityList[0], function (num, key) {
       return key.replace(/([A-Z])/g, function ($1) {
-        return ' ' + $1;
+        return " " + $1;
       });
     });
     // Uppercase the first letter.
@@ -181,7 +188,7 @@ export class AppComponent implements OnInit {
     });
     // Remove the order options we don't want. In the instructions you only want "Rank", "Name", and "Age".
     // Why not the other options? Obviously we don't want "Net Worth convert.
-    const remove = ['Net Worth', 'Country', 'Net Worth Convert'];
+    const remove = ["Net Worth", "Country", "Net Worth Convert"];
     for (let i = 0; i < remove.length; i++) {
       this.orders = arrayRemove(this.orders, remove[i]);
     }
@@ -201,7 +208,7 @@ export class AppComponent implements OnInit {
     const conversion = this.dollarValues[key].value;
     this.celebrityList.map(function (value) {
       // Convert Net Worth to currently selected option.
-      value['netWorthConvert'] = value['netWorth'] * conversion;
+      value["netWorthConvert"] = value["netWorth"] * conversion;
       return value;
     });
   }
@@ -229,19 +236,23 @@ export class AppComponent implements OnInit {
   public filter() {
     // If "Show All" is selected obviously we don't want to use that value.
     let birthClause = this.birthClause;
-    if (this.birthClause === 'Show All') {
-      birthClause = '';
+    if (this.birthClause === "Show All") {
+      birthClause = "";
     }
     // Get the search clause here. Otherwise I would have to bind the scope. Just easier
     const searchClause = this.searchClause;
     this.celebrityClone = _.filter(this.celebrityList, function (obj) {
       // Filter by "Name", "Net Worth", "Age", and "Birthplace" but also include the selected birthplace option.
       return (
-        obj.name.toLowerCase().includes(searchClause.toLowerCase()) ||
-        obj.netWorthConvert.toLocaleString().toLowerCase().includes(searchClause.toLowerCase()) ||
-        obj.age.toLowerCase().includes(searchClause.toLowerCase()) ||
-        obj.country.toLowerCase().includes(searchClause.toLowerCase())
-      ) && obj.country.includes(birthClause);
+        (obj.name.toLowerCase().includes(searchClause.toLowerCase()) ||
+          obj.netWorthConvert
+            .toLocaleString()
+            .toLowerCase()
+            .includes(searchClause.toLowerCase()) ||
+          obj.age.toLowerCase().includes(searchClause.toLowerCase()) ||
+          obj.country.toLowerCase().includes(searchClause.toLowerCase())) &&
+        obj.country.includes(birthClause)
+      );
     });
   }
 }
